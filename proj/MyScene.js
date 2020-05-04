d4aca7b4a68a228115097198e06beb6f295cb1a0
MechJM <matosjoaops@protonmail.com>
2020-05-04 22:45:28 +0100

    Changed initial camera position and added screenshot

--- proj/MyScene.js
+++ proj/MyScene.js
@@ -71,7 +71,7 @@ class MyScene extends CGFscene {
         this.lights[0].update();
     }
     initCameras() {
-        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(40, 60, 40), vec3.fromValues(0, 0, 0));
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(25, 35, 25), vec3.fromValues(0, 0, 0));
     }
     setDefaultAppearance() {
         this.setAmbient(0.2, 0.4, 0.8, 1.0);
